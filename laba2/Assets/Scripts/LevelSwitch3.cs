75ee963edc0960127419cfa8bf58a4737d3b8046
sad-ukey <146202627+sad-ukey@users.noreply.github.com>
2024-12-13 17:48:14 +0300

    добавил переключение уровней

---
+++ laba2/Assets/Scripts/LevelSwitch3.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LevelSwitch3 : MonoBehaviour
+{
+    public GameObject levelToActivate; // Укажите уровень, который нужно активировать
+    public GameObject levelToDeactivate; // Укажите уровень, который нужно деактивировать
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Здесь мы проверяем, что в триггер попал игрок
+        if (other.CompareTag("Player"))
+        {
+            // Активируем уровень 5, если попали в MoveTo5
+            if (gameObject.name == "MoveTo5")
+            {
+                if (levelToActivate != null)
+                {
+                    levelToActivate.SetActive(true); // Показываем Level 5
+                }
+            }
+
+            // Деактивируем уровень 4, если попали в Remove1
+            if (gameObject.name == "Remove4")
+            {
+                if (levelToDeactivate != null)
+                {
+                    levelToDeactivate.SetActive(false); // Прячем Level 4
+                }
+            }
+        }
+    }
+}
